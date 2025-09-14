a04ceae30c4508daa7d0ab6822681c8cd4d9eb63
赤井幹之 <atheist.r.e@gmail.com>
2025-09-14 13:05:32 +0900

    削除、編集ボタンの追加

--- blog-rehab/resources/js/components/PostTable.vue
+++ blog-rehab/resources/js/components/PostTable.vue
@@ -1,11 +1,18 @@
 <template>
   <v-app>
     <v-main>
-      <v-data-table
-        :headers="headers"
-        :items="posts"
-        item-key="id"
-      />
+        <v-data-table
+            :headers="headers"
+            :items="posts"
+            item-key="id"
+            :items-per-page="10"
+            hover
+        >
+        <template #item.actions="{ item }">
+            <v-btn size="small" color="primary" class="mr-2">編集</v-btn>
+            <v-btn size="small" color="error" variant="outlined">削除</v-btn>
+        </template>
+      </v-data-table>
     </v-main>
   </v-app>
 </template>
@@ -21,6 +28,7 @@ export default {
         { title: 'ID', key: 'id' },
         { title: 'タイトル', key: 'title' },
         { title: '本文', key: 'body' },
+        { title: '操作', key: 'actions', sortable: false, align: 'center' },
       ]
     }
   }
