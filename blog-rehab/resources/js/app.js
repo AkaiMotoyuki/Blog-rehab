d09a4118b451d70aabfb1437346d08488b973d38
赤井幹之 <atheist.r.e@gmail.com>
2025-09-14 16:14:05 +0900

    退避用

--- blog-rehab/resources/js/app.js
+++ blog-rehab/resources/js/app.js
@@ -1,24 +1,7 @@
 import './bootstrap';
-import { createApp } from 'vue'
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import VueBadge from './components/VueBadge.vue'
-import PostTable from './components/PostTable.vue'
 
-const app = createApp(VueBadge, { label: 'Vue OK' })
-const vuetify = createVuetify({
-    components,
-    directives,
-})
-const el = document.getElementById('app')
-const payload = JSON.parse(el.dataset.posts)
-const posts = Array.isArray(payload) ? payload : (payload?.data ?? [])
-app.use(vuetify)
-app.component('vue-badge', VueBadge)
-app.mount('#test')
+import Alpine from 'alpinejs';
 
-createApp(PostTable, { posts })
-  .use(vuetify)
-  .mount('#app')
+window.Alpine = Alpine;
+
+Alpine.start();
